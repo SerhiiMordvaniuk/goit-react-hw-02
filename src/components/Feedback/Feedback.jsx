a321d689f0aaba3da7515126f81754d5e305a62c
Serhii Mordvaniuk <sm636332@gmail.com>
2024-12-11 18:51:26 +0100

    styles

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -6,14 +6,14 @@ const Feedback = ({ good, neutral, bad, totalFeedback }) => {
 
     return (
         <>
-            <div>{ !totalFeedback ? `No feedback yet` : `` }</div>
+            <p className={s.feedback}>{ !totalFeedback ? `No feedback yet` : `` }</p>
             {totalFeedback > 0 && (
                 <ul className={s.list}>
                     <li>Good: {good}</li>
                     <li>Neutral: {neutral}</li>
                     <li>Bad: {bad}</li>
                     <li>Total: { totalFeedback}</li>
-                    <li>{ Math.round((good / totalFeedback) * 100)}% </li>
+                    <li>Positive: { Math.round((good / totalFeedback) * 100)}% </li>
                 </ul>
             )}
         </>
