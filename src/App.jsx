0b644b64860d2a474b9a505097c10acf0ef05eb6
Serhii Mordvaniuk <sm636332@gmail.com>
2024-12-12 15:55:11 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
-import Description from './components/Title/Description';
+import Description from './components/Description/Description';
 import Options from "./components/Options/Options"
 import Feedback from './components/Feedback/Feedback';
+import Notification from "./components/Notification/Notification"
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
   });
 
   const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+  const PositiveFeedback = Math.round((feedbacks.good / totalFeedback) * 100)
 
 
   const updateFeedback = (feedbackType) => {
@@ -24,8 +26,6 @@ function App() {
       ...feedbacks,
     [feedbackType]: feedbacks [feedbackType] + 1
     })
-    console.log(feedbacks);
-    console.log(JSON.stringify(feedbacks));
     
     
   }
@@ -36,7 +36,7 @@ function App() {
 
   useEffect(() => {
     window.localStorage.setItem("feedbacks", JSON.stringify(feedbacks))
-  })
+  }, [feedbacks])
 
   return (
     <>
@@ -46,12 +46,16 @@ function App() {
         totalFeedback={totalFeedback}
         clear={clearFeedback}
       />
-      <Feedback
+      <>{totalFeedback > 0
+        ? <Feedback
         good={feedbacks.good}
         neutral={feedbacks.neutral}
         bad={feedbacks.bad}
         totalFeedback={totalFeedback}
+        positive={PositiveFeedback}
       />
+        : <Notification
+        totalFeedback={ totalFeedback} /> }</>
     </>
   )
 }
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,21 +1,18 @@
 import s from './Feedback.module.css'
 
 
-const Feedback = ({ good, neutral, bad, totalFeedback }) => {
+const Feedback = ({ good, neutral, bad, totalFeedback, positive }) => {
     
 
     return (
         <>
-            <p className={s.feedback}>{ !totalFeedback ? `No feedback yet` : `` }</p>
-            {totalFeedback > 0 && (
-                <ul className={s.list}>
-                    <li>Good: {good}</li>
-                    <li>Neutral: {neutral}</li>
-                    <li>Bad: {bad}</li>
-                    <li>Total: { totalFeedback}</li>
-                    <li>Positive: { Math.round((good / totalFeedback) * 100)}% </li>
-                </ul>
-            )}
+            <ul className={s.list}>
+                <li>Good: {good}</li>
+                <li>Neutral: {neutral}</li>
+                <li>Bad: {bad}</li>
+                <li>Total: { totalFeedback}</li>
+                <li>Positive: {positive}% </li>
+            </ul>
         </>
     )
 }
